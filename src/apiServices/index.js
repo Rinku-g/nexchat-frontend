2b9ebfd0ceabf3af8ed75bfe9a5cd611fea81c62
osndl.rinkug <osndl.rinkug@aicofindia.com>
2026-07-28 14:18:12 +0530

    update production url

--- src/apiServices/index.js
+++ src/apiServices/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://nexchat-backend-production-d51b.up.railway.app/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
