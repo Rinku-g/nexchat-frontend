1c136601d4142718a201b4377b5adec80ad0f3ff
osndl.rinkug <osndl.rinkug@aicofindia.com>
2026-07-30 14:20:58 +0530

    update the api url

--- src/apiServices/index.js
+++ src/apiServices/index.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_LOCAL_URL,
+  baseURL:
+    import.meta.env?.MODE === "development"
+      ? import.meta.env.VITE_API_LOCAL_URL
+      : import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
